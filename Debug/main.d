9ecffe4c2b4fc3f1d5ffa71c0951c0e400f9a74e
Madhav Rajesh <madmusic2020@gmail.com>
2024-02-22 11:14:07 -0800

    Initial commit

---
+++ Debug/main.d
@@ -0,0 +1,117 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h
+main.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+main.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+main.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h
+main.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+main.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+main.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/simplelink.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/user.h
+main.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h
+main.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/cc_pal.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink_extlib/provisioninglib/provisioning_api.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/simplelink.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/source/objInclusion.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/trace.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/fs.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/socket.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/netapp.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/wlan.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/device.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/netcfg.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/wlan_rx_filters.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/source/nonos.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/inc/hw_types.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/inc/hw_ints.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/rom.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/rom_map.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/rom_patch.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/interrupt.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/prcm.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/utils.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/uart.h
+main.obj: ../pinmux.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/example/common/gpio_if.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/example/common/common.h
+main.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/example/common/uart_if.h
+
+../main.c:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/simplelink.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/user.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/cc_pal.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink_extlib/provisioninglib/provisioning_api.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/simplelink.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/source/objInclusion.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/trace.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/fs.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/socket.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/netapp.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/wlan.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/device.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/netcfg.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/wlan_rx_filters.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/source/nonos.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/inc/hw_types.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/inc/hw_ints.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/rom.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/rom_map.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/rom_patch.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/interrupt.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/prcm.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/utils.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/uart.h:
+
+../pinmux.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/example/common/gpio_if.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/example/common/common.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/example/common/uart_if.h:
+
---
+++ Debug/network_common.d
@@ -0,0 +1,69 @@
+# FIXED
+
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/example/common/network_common.c
+network_common.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h
+network_common.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+network_common.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+network_common.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/simplelink.h
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/user.h
+network_common.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h
+network_common.obj: /Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/cc_pal.h
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink_extlib/provisioninglib/provisioning_api.h
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/simplelink.h
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/source/objInclusion.h
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/trace.h
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/fs.h
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/socket.h
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/netapp.h
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/wlan.h
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/device.h
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/netcfg.h
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/wlan_rx_filters.h
+network_common.obj: /Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/source/nonos.h
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/example/common/network_common.c:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/simplelink.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/user.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h:
+
+/Applications/TI/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/cc_pal.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink_extlib/provisioninglib/provisioning_api.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/simplelink.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/source/objInclusion.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/trace.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/fs.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/socket.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/netapp.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/wlan.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/device.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/netcfg.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/include/wlan_rx_filters.h:
+
+/Applications/TI/lib/cc3200sdk_1.5.0/cc3200-sdk/simplelink/source/nonos.h:
+
